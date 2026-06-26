d81810b2e616f7791abfbd9ffd0a45a0211c49d4
subodh <subodh.b@indibasolutions.com>
2026-06-26 18:16:46 +0530

    Sales Order and Campaign url fields added

--- binary_int/api/create_campaign.py
+++ binary_int/api/create_campaign.py
@@ -1,4 +1,21 @@
 import frappe
 
 def create_campaign(doc, method):
+
     frappe.msgprint(" HI there ...!")
+
+    url = "https://api-dev.binaryintent.com/api/campaign-manager/campaign"
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
